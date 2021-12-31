acc76f43971df4846e43f4986ab11e4fbb20a240
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2021-12-31 13:18:36 -0300

    Criando arquivos .txt

--- arquivos/gerador_bhaskara.py
+++ arquivos/gerador_bhaskara.py
@@ -1,59 +1,97 @@
-add = []
-x1 = x2 = 0
+import os
 
 def verificar_raiz(delta):
 
     if delta >= 0:
-
-        raiz = verificar_dizima(delta**0.5)
-
-        if raiz != None:
-
-            return raiz
-
+        return verificar_dizima(delta**0.5)
+  
 
 def verificar_dizima(num):
 
-    if num == round(num, 2):
-
+    if num == round(num, quantV):
         return num
 
 
-for a in range(-9, 10):
-    for b in range(-9, 10):
-        for c in range(-9, 10):
+add = []
+x1 = x2 = cont = 0
 
-            v = {'a': a, 'b': b, 'c': c}
+menorP, maiorP, quantV = -9, 9, 2
+'''
+menorP = int(input('Menor número possivel: '))
+maiorP = int(input('Maior número possivel: '))
+quantV = int(input('Resultados com até quantos números após a virula? '))
+'''
+for a in range(menorP, maiorP + 1):
+    for b in range(menorP, maiorP + 1):
+        for c in range(menorP, maiorP + 1):
 
-            delta = (v['b'])**2 -4 * v['a'] * v['c']
+            cont += 1
 
-            v['d'] = delta
+            v = {'a': a, 'b': b, 'c': c, 'd': None, 'x1': 0, 'x2': 0}
 
-            raiz = verificar_raiz(delta)
+            v['d'] = (v['b'])**2 -4 * v['a'] * v['c']
+
+            raiz = verificar_raiz(v['d'])
 
             if raiz != None:
 
-                try:
+                for c in range(1, 3):
 
-                    x1 = (- v['b'] + raiz) / (2 * v['a'])
-                    x2 = (- v['b'] - raiz) / (2 * v['a'])
+                    if c == 1:
 
-                except ZeroDivisionError:
+                        part1 = - v['b'] + raiz
 
-                    pass
+                    elif c == 2:
 
-                if None not in (x1, x2):
+                        part1 = - v['b'] - raiz
 
-                    v['x1'] = verificar_dizima(x1)
+                    part2 = 2 * v['a']
 
-                    v['x2'] = verificar_dizima(x2)
+                    if 0 not in (part1, part2):
 
-                    if None not in (v['x1'], v['x2']):
+                        v[f'x{c}'] = verificar_dizima(part1/part2)
 
-                        add.append(v)
+                if None not in (v['x1'], v['x2']):
 
-print(add[789])
-                
+                    add.append(v)
+while True:
 
-    
+    quest = input(f'Digite um valor de 0 a {len(add)}: ')
 
+    if quest == '':
+        
+        break
+
+    try:
+
+        quest = int(quest)
+
+    except ValueError:
+
+        print('Digite um número!')
+
+    else:
+
+        if quest > len(add) or quest < 0:
+
+            print('Valor invalido!')
+
+        else:
+
+            print(add[quest])
+
+local = f'arquivos/QeR/Menor {menorP} | Maior {maiorP} | PosVirg {quantV} | Tot {len(add)}'
+
+os.makedirs(local, exist_ok=True)
+
+perg = open(f'{local}/Questões Bhaskara.txt', 'w')
+resp = open(f'{local}/Respostas Bhaskara.txt', 'w')
+
+print(os.getcwd())
+
+for c in range(len(add)):
+
+    perg.write(f'{c+1}) a = {add[c]["a"]} | b = {add[c]["b"]} | c = {add[c]["c"]}\n\n')
+    resp.write(f'{c+1}) d = {add[c]["d"]} | x1 = {add[c]["x1"]} | x2 = {add[c]["x2"]}\n\n')
+
+print('Menor -9 | Maior 9 | PosVirg 2 | Tot 1021')
